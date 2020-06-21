939a8ee6a67f4f572d31840c45eab7d36dae85be
Elliot Bohlen <eebohlen@gmail.com>
2020-06-21 16:33:52 -0400

    bugs advice

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ var append1Function = setInterval(function(){
 
  var pic4 = `<div class="patternunit" style="
 
-   "><img src="images/adv/d2.png"></div>`;
+   "><img src="images/adv/d2copy.jpg"></div>`;
  document.getElementById("box4").insertAdjacentHTML("beforeend", pic4);
 
  var pic5 = `<div class="patternunit" style="
