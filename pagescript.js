4ce5c0db04cb5fc43d02db027e38587df9b26b4a
elliotbohlen <bohle000@newschool.edu>
2021-06-14 21:24:13 -0400

    nothing works

--- pagescript.js
+++ pagescript.js
@@ -1,33 +1,57 @@
-// console.log("working")
-// var myIndex = 0;
-// carousel();
-
-// function carousel() {
-//     var i;
-//     var x = document.getElementsByClassName("projimages");
-//     for (i = 0; i < x.length; i++) {
-//         x[i].style.display = "none";
-//     }
-//     myIndex++;
-//     if (myIndex > x.length) {
-//         myIndex = 1
-//     }
-//     x[myIndex - 1].style.display = "block";
-//     setTimeout(carousel, 400); // Change image every 2 seconds
-
-// }
-
-
-
 //doesnt have to be a toggle?
 $(document).ready(function() {
     $("#logo").css('display', 'block')
     $("#thesispg").css('display', 'none')
-    $("#mirrorworldpg").css('display', 'none')
+    $("#mirrorpg").css('display', 'none')
     $("#collabpg").css('display', 'none')
     $("#surfinpg").css('display', 'none')
+    $("#thesisdesc").css('display', 'none')
+    $("#mirrordesc").css('display', 'none')
+    $("#collabdesc").css('display', 'none')
+    $("#surfindesc").css('display', 'none')
+        // $(".info").hide()
+
+    // $(".projects").click(function() {
+    //     // $("#wrapper").css({
+    //     //     'grid-template-columns': '50px 400px 1fr'
+    //     // })
+    //     $("#link").css({
+    //         'opacity': '1',
+    //         // 'grid-column': '1'
+    //     })
+    //     $(".projects").css({
+    //         'opacity': '0',
+    //         // 'grid-column': '2'
+    //     })
+    // });
+
+    // //reverses
+    // $("#link").click(function() {
+
+    //     $("#link").css({
+    //         'opacity': '0',
+    //         // 'grid-column': '2'
+    //     })
+    //     $(".projects").css({
+    //         'opacity': '1',
+    //         // 'grid-column': '1'
+    //     })
+    // });
 
 
+    // $('#link').hide();
+    // $('#link, .projects').click(function() {
+    //     $('#link, .projects').toggle();
+    //     // $("#wrapper").css({
+    //     //     'grid-template-columns': '50px 50px 400px 1fr'
+    //     // })
+    // });
+    // $(".projects").click(function() {
+    //     $("#wrapper").css({
+    //         'grid-template-columns': '0px 50px 400px 1fr'
+    //     })
+
+    // });
 
 
 
@@ -35,35 +59,75 @@ $(document).ready(function() {
         $("#thesispg").toggle();
         $("#logo").css('display', 'none')
         $("#thesispg").css('display', 'block')
+        $("#thesisdesc").css('display', 'block')
         $("#mirrorpg").css('display', 'none')
+        $("#mirrordesc").css('display', 'none')
         $("#collabpg").css('display', 'none')
+        $("#collabdesc").css('display', 'none')
         $("#surfinpg").css('display', 'none')
+        $("#surfindesc").css('display', 'none')
+
+        // $("#wrapper").css({
+        //     'grid-template-columns': '50px 400px 1fr',
+
+        // })
+
+        // $(".projects").css({
+
+        //     'width': '50px',
+        //     'justify-content': 'center',
+        //     'transition': 'width .8s, justify-content .8s'
+        // })
+
+        // $(".proj").css('display', "none", )
+        // $(".info").css({ 'display': "block", 'transition': 'display .8s,' })
+
+
+        // $(".projects").text("arrow");
+
+        // $("#.projects").toggleClass("info")
+        // $('.projects').removeAttr('class');
+
+
+
     });
 
     $("#mirror").click(function() {
         $("#mirrorpg").toggle();
         $("#logo").css('display', 'none')
         $("#thesispg").css('display', 'none')
+        $("#thesisdesc").css('display', 'none')
         $("#mirrorpg").css('display', 'block')
+        $("#mirrordesc").css('display', 'block')
         $("#collabpg").css('display', 'none')
+        $("#collabdesc").css('display', 'none')
         $("#surfinpg").css('display', 'none')
+        $("#surfindesc").css('display', 'none')
     });
 
     $("#collab").click(function() {
         $("#collabpg").toggle();
         $("#logo").css('display', 'none')
         $("#thesispg").css('display', 'none')
+        $("#thesisdesc").css('display', 'none')
         $("#mirrorpg").css('display', 'none')
+        $("#mirrordesc").css('display', 'none')
         $("#collabpg").css('display', 'block')
+        $("#collabdesc").css('display', 'block')
         $("#surfinpg").css('display', 'none')
+
     });
 
     $("#surfin").click(function() {
         $("#surfinpg").toggle();
         $("#logo").css('display', 'none')
         $("#thesispg").css('display', 'none')
+        $("#thesisdesc").css('display', 'none')
         $("#mirrorpg").css('display', 'none')
+        $("#mirrordesc").css('display', 'none')
         $("#collabpg").css('display', 'none')
+        $("#collabdesc").css('display', 'none')
         $("#surfinpg").css('display', 'block')
+        $("#surfindesc").css('display', 'block')
     });
 });
